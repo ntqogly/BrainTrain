c6d09cd5511d40a407b096a80a4551bbe5752828
Qarapapax <aldunsar@yahoo.com>
2023-06-21 22:20:13 +0300

    View Binding and rest of resources added

---
+++ app/src/main/java/com/example/braintrain/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,28 @@
+package com.example.braintrain.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.braintrain.databinding.FragmentChooseLevelBinding
+import java.lang.RuntimeException
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding = null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
