e96a7c257411663408aca009295fa01ef8939ff1
Qarapapax <aldunsar@yahoo.com>
2023-07-09 19:05:32 +0400

    Implementation navigation

--- app/src/main/java/com/example/braintrain/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/braintrain/presentation/ChooseLevelFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.braintrain.R
 import com.example.braintrain.databinding.FragmentChooseLevelBinding
 import com.example.braintrain.domain.entity.Level
-import java.lang.RuntimeException
 
 class ChooseLevelFragment : Fragment() {
 
@@ -39,9 +39,11 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME).commit()
+//        requireActivity().supportFragmentManager.beginTransaction()
+//            .replace(R.id.main_container, GameFragment.newInstance(level))
+//            .addToBackStack(GameFragment.NAME).commit()
+        val args = Bundle().apply { putParcelable(GameFragment.KEY_LEVEL, level) }
+        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment, args)
     }
 
     companion object {
--- app/src/main/java/com/example/braintrain/presentation/GameFragment.kt
+++ app/src/main/java/com/example/braintrain/presentation/GameFragment.kt
@@ -9,10 +9,10 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.braintrain.R
 import com.example.braintrain.databinding.FragmentGameBinding
 import com.example.braintrain.domain.entity.GameResult
-import com.example.braintrain.domain.entity.GameSettings
 import com.example.braintrain.domain.entity.Level
 
 class GameFragment : Fragment() {
@@ -121,13 +121,17 @@ class GameFragment : Fragment() {
     }
 
     private fun launchGameFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction().replace(
-            R.id.main_container, GameFinishedFragment.newInstance(gameResult)
-        ).addToBackStack(null).commit()
+//        requireActivity().supportFragmentManager.beginTransaction().replace(
+//            R.id.main_container, GameFinishedFragment.newInstance(gameResult)
+//        ).addToBackStack(null).commit()
+        val args = Bundle().apply {
+            putParcelable(GameFinishedFragment.KEY_GAME_RESULT, gameResult)
+        }
+        findNavController().navigate(R.id.action_gameFragment_to_gameFinishedFragment, args)
     }
 
     companion object {
-        private const val KEY_LEVEL = "level"
+        const val KEY_LEVEL = "level"
         const val NAME = "GameFragment"
         fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
